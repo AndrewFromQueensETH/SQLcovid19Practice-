a1f665c69122657fbd8a8812584d5dd1551736fb
AndrewFromQueensETH <100327548+AndrewFromQueensETH@users.noreply.github.com>
2023-05-02 10:57:10 -0700

    Delete SQL COVID STATS PORTFLOIO PROJECT.sql

--- SQL COVID STATS PORTFLOIO PROJECT.sql
+++
@@ -1,4 +0,0 @@
-select   MAX(total_cases) as Highestinfectioncount,  Max((total_cases/population))*100 as PercentPopulationInfected,
- Max((total_vaccinations/population))* 100 as numberofvaccinated 
-From CovidDeaths$
-Order by 1,2   
